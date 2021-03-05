3580bd09795815b6b755871be08125f34999f8e6
Gabriel <Gabriel.Senekovic@gmail.com>
2021-03-05 01:52:17 +0100

    Pick up key and unlock door

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,14 +12,32 @@ public class PlayerController : MonoBehaviour
 
     public bool moving;
 
+    public Interactable currentInteractable;
+
+    int keys;
+
+    Text text;
+
     private void Start() 
     {
         movementDirection = Vector2.zero;
         rigidbody = GetComponent<Rigidbody>();
         moving = false;
+        text = GetComponentInChildren<Text>();
+        keys = 0;
     }
     private void Update() 
     {
+        text.text = currentInteractable ? currentInteractable.name: "";
+        if(currentInteractable)
+        {
+            if(Input.GetKey(KeyCode.Space) && (currentInteractable.pickup || (!currentInteractable.pickup && keys > 0)))
+            {
+                currentInteractable.gameObject.SetActive(false);
+                keys = currentInteractable.pickup ? keys + 1: 0;
+                currentInteractable = null;
+            }
+        }
         if(Input.GetKey(KeyCode.W))
         {
             movementDirection.y = 1;
@@ -52,6 +71,35 @@ public class PlayerController : MonoBehaviour
             }
             moving = false;
             movementDirection = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.GetComponent<Interactable>())
+        {
+            currentInteractable = other.gameObject.GetComponent<Interactable>();
+        }
+    }
+    private void OnCollisionExit(Collision other) 
+    {
+        if(other.gameObject.GetComponent<Interactable>())
+        {
+            currentInteractable = null;
+        }
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.GetComponent<Interactable>())
+        {
+            currentInteractable = other.gameObject.GetComponent<Interactable>();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.GetComponent<Interactable>())
+        {
+            currentInteractable = null;
         }
     }
 }
