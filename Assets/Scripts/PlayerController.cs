87f98da6dba362ea01a2b4b88fe07176be66b79d
Gabriel <Gabriel.Senekovic@gmail.com>
2021-03-15 15:18:15 +0100

    Merge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,64 +7,102 @@ public class PlayerController : MonoBehaviour
     new Rigidbody rigidbody;
     public Vector2 movementDirection;
     public int speed;
-    public float maxSpeed;
 
     public float jumpStrength;
 
-    public bool moving;
+    public Interactable currentInteractable;
+
+    int keys;
+
+    bool jumping;
+
+    Text text;
 
     private void Start() 
     {
         movementDirection = Vector2.zero;
         rigidbody = GetComponent<Rigidbody>();
-        moving = false;
+        text = GetComponentInChildren<Text>();
+        keys = 0;
     }
     private void Update() 
     {
+        text.text = currentInteractable ? currentInteractable.name: "";
+        if(currentInteractable)
+        {
+            if(Input.GetKeyDown(KeyCode.Q) && (currentInteractable.pickup || (!currentInteractable.pickup && keys > 0)))
+            {
+                currentInteractable.UseKey(ref currentInteractable, ref keys);
+            }
+        }
         if(Input.GetKey(KeyCode.W))
         {
             movementDirection.y = 1;
-            moving = true;
         }
         else if(Input.GetKey(KeyCode.S))
         {
             movementDirection.y = -1;
-            moving = true;
         }
         if(Input.GetKey(KeyCode.D))
         {
             movementDirection.x = 1;
-            moving = true;
         }
         else if(Input.GetKey(KeyCode.A))
         {
             movementDirection.x = -1;
-            moving = true;
         }
         movementDirection.Normalize();
+        if(Input.GetKeyDown(KeyCode.Space) && Grounded())
+        {
+            jumping = true;
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            jumping = false;
+        }
     }
     private void FixedUpdate() 
     {
-        if(Input.GetButton("Jump") && Grounded())
+        Vector3 force = CameraMovement.GetCameraDirection() * new Vector3(movementDirection.x * speed, 0, movementDirection.y * speed);
+        rigidbody.velocity = new Vector3(force.x, rigidbody.velocity.y, force.z);
+        movementDirection = Vector2.zero;
+        if(jumping)
         {
             rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpStrength, rigidbody.velocity.z);
         }
+    }
 
-
-        if(moving)
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.GetComponent<Interactable>() && other.gameObject.GetComponent<Interactable>().interactable)
         {
-            Vector3 force = CameraMovement.GetCameraDirection() * new Vector3(movementDirection.x * speed, 0, movementDirection.y * speed);
-            if(rigidbody.velocity.magnitude + force.magnitude <= maxSpeed)
-            {
-                rigidbody.AddForce(force);
-            }
-            moving = false;
-            movementDirection = Vector2.zero;
+            currentInteractable = other.gameObject.GetComponent<Interactable>();
+        }
+    }
+    private void OnCollisionExit(Collision other) 
+    {
+        if(other.gameObject.GetComponent<Interactable>())
+        {
+            currentInteractable = null;
+        }
+    }
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.GetComponent<Interactable>() && other.gameObject.GetComponent<Interactable>().interactable)
+        {
+            currentInteractable = other.gameObject.GetComponent<Interactable>();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.GetComponent<Interactable>())
+        {
+            currentInteractable = null;
         }
     }
 
     private bool Grounded()
     {
-        return Physics.Raycast(transform.position, Vector3.down, .7f);
+        return Physics.Raycast(transform.position, Vector3.down, 1);
     }
 }
