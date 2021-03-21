2ac136624b0f56b94851d01496a713766dc2e4e7
Mirriu <Irma.Andersson.0465@student.uu.se>
2021-03-21 20:26:04 +0100

    Keycards, movement and camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -13,7 +13,6 @@ public class CameraMovement : MonoBehaviour
     public GameObject player_model;
 
     public float height;
-    public float rotationSideways;
     public int rotationSpeed;
 
     private void Start() 
@@ -24,7 +23,7 @@ public class CameraMovement : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        //stop removing this >:(
+        
     }
 
     private void Update() 
@@ -32,22 +31,34 @@ public class CameraMovement : MonoBehaviour
         //if(Input.GetKey(KeyCode.LeftArrow))
         
         {
-            Rotate(Input.GetAxis("Mouse X") * rotationSpeed);
+           Rotate(Input.GetAxis("Mouse X") * rotationSpeed, Input.GetAxis("Mouse Y") * rotationSpeed);
+           
 
-        //    Rotate(rotationSpeed);
-        //    rotationSideways+= rotationSpeed;
-        //}
-        //else if(Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    Rotate(-rotationSpeed);
-        //    rotationSideways-= rotationSpeed;
+
+            //    Rotate(rotationSpeed);
+            //    rotationSideways+= rotationSpeed;
+            //}
+            //else if(Input.GetKey(KeyCode.RightArrow))
+            //{
+            //    Rotate(-rotationSpeed);
+            //    rotationSideways-= rotationSpeed;
         }
         transform.position = new Vector3(transform.position.x, player.transform.position.y + height, transform.position.z);
     }
-    void Rotate(float speed)
+    void Rotate(float x, float y)
     {
-        transform.RotateAround(new Vector3(player.transform.position.x, 0, player.transform.position.z), Vector3.up, speed);
-        player_model.transform.RotateAround(new Vector3(player.transform.position.x, 0, player.transform.position.z), Vector3.up, speed);
+
+        y = Mathf.Clamp(y,-90f, 90f);
+
+        //transform.RotateAround(new Vector3(player.transform.position.x, 0, player.transform.position.z), Vector3.up, x);
+        transform.RotateAround(new Vector3(player.transform.position.x, 0, player.transform.position.z), Vector3.right, y);
+        player_model.transform.RotateAround(new Vector3(player.transform.position.x, 0, player.transform.position.z), Vector3.up, x);
+
+
+        
+
+        //transform.RotateAround(new Vector3(player.transform.position.x, 0, player.transform.position.z), Vector3.up, speed);
+        //player_model.transform.RotateAround(new Vector3(player.transform.position.x, 0, player.transform.position.z), Vector3.up, speed);
     }
     public static Quaternion GetCameraDirection()
     {
